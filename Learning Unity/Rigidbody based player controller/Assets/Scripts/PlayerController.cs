fad7c5a093eb517b7163ae2e54b1bdfdcdf3f7de
btwyourethebest-wq <btwyourethebest@gmail.com>
2026-04-20 11:08:25 +0300

    Updated "Rigidbody based player controller"

--- Learning Unity/Rigidbody based player controller/Assets/Scripts/PlayerController.cs
+++ Learning Unity/Rigidbody based player controller/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:804b9a32fb2a0c60d53796a5a066da0f5e0b56281913ee414b3c382bcbbe6fbe
-size 533
+oid sha256:1fada645d909d50fa34c3d707f8203421a7f428e0359c2abe803a277d05e7d53
+size 640
